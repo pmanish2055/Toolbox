5c286197cdd743cba2735737f58cc48126621869
Manish Pathak <pmanish2055@gmail.com>
2025-07-25 12:48:15 +0545

    Initial Push

---
+++ core/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+
+def home (req):
+    return render(req,'home.html')
+
+def imagetool (req):
+    return render(req,'components/imagetool.html')
+
+def texttool (req):
+    return render(req,'components/texttool.html') 
+
+def mathtool (req):
+    return render(req,'components/mathtool.html') 
+
+def webtool (req):
+    return render(req,'components/webtool.html') 
+
+def codeutilitiestool (req):
+    return render(req,'components/codeutilitiestool.html') 
+
+def misctool (req):
+    return render(req,'components/misctool.html') 
